852f052dfb5f259b51366554242e45f9edbff7d6
Rodeo <tdskywalker@gmail.com>
2013-07-26 17:22:50 +0000

    git-svn-id: svn://svn.handbrake.fr/HandBrake/branches/qsv@5667 b64f7644-9d1e-0410-96f1-a4d463321fa5

--- test/test.c
+++ test/test.c
@@ -1823,7 +1823,6 @@ static int HandleEvents( hb_handle_t * h )
 
             if (vcodec == HB_VCODEC_QSV_H264)
             {
-                int qsv_deinterlace;
                 filter = hb_filter_init(HB_FILTER_QSV_PRE);
                 hb_add_filter(job, filter, NULL);
                 filter = hb_filter_init(HB_FILTER_QSV_POST);
@@ -1835,9 +1834,9 @@ static int HandleEvents( hb_handle_t * h )
                  * OR
                  * - deinterlace_opt == "32"
                  */
-                qsv_deinterlace = (deinterlace &&
-                                   (!deinterlace_opt ||
-                                    !strcmp(deinterlace_opt, "32")));
+                int qsv_deinterlace = (deinterlace &&
+                                       (!deinterlace_opt ||
+                                        !strcmp(deinterlace_opt, "32")));
                 filter_str = hb_strdup_printf("%d:%d:%d:%d:%d:%d_dei:%s",
                                               job->width,   job->height,
                                               job->crop[0], job->crop[1],
